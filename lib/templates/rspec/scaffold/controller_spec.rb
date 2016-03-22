753d6de95f9ff7ea62b7839cdb8e54c53e14aea0
Dmitri Koulikoff <dima@koulikoff.ru>
2016-03-22 17:27:00 +0300

    Assingment of variavle attribute is widened

--- lib/templates/rspec/scaffold/controller_spec.rb
+++ lib/templates/rspec/scaffold/controller_spec.rb
@@ -17,7 +17,7 @@ describe <%= controller_class_name %>Controller, type: :controller do
   # This should return the minimal set of attributes required to create a valid
   # <%= class_name %>. As you add validations to <%= class_name %>, be sure to
   # adjust the attributes here as well. The list could not be empty.
-<% attribute = attributes.detect{|a| attributes.detect{|a| a.name == 'name'} || attributes.detect{|a| a.name == 'title'} || attributes.first} -%>
+<% attribute = attributes.detect{|a| a.name == 'name' || a.name == 'title' || a.name == 'code' || a.name =~ /name/ || a.name =~ /title/ || attributes.first} -%>
 <% attribute_name = attribute.respond_to?(:column_name) ? attribute.column_name : attribute.name -%>
 <% if Rails.application.config.generators.options[:rails][:fixture_replacement] == :factory_girl -%>
 <% factory_girl = true -%>
@@ -30,6 +30,7 @@ describe <%= controller_class_name %>Controller, type: :controller do
 <% end -%>
 
   let(:invalid_attributes) do
+    # {<%= attribute_name %>: ''}
     skip("Add a hash of attributes invalid for your model")
   end
 
