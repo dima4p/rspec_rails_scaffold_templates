36f8a5052e8f5870c34f6dbca0d79e8aca31b366
Dmitry Kulikov <dima@koulikoff.ru>
2020-06-05 12:01:31 +0200

    requiest_spec is fixed

--- lib/rspec_rails_scaffold_templates.rb
+++ lib/rspec_rails_scaffold_templates.rb
@@ -1,3 +1,5 @@
+require 'rails/generators'
+
 if defined?(Rails)
   require 'rspec_rails_scaffold_templates/generators/scaffold_generator'
   require 'rspec_rails_scaffold_templates/generators/integration_generator'
